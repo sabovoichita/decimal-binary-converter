adc45c030f193cdc23c9bf46a4cbc99e119abefd
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-20 15:49:37 +0100

    108. Finally, set the textContent property of result equal to calling decimalToBinary() with 5 as an argument. After this, test out your code by entering the number 5 into the number input and clicking the Convert button.

--- index.js
+++ index.js
@@ -49,7 +49,9 @@ const showAnimation = () => {
       document.getElementById(obj.inputVal).remove();
     }, obj.removeElDelay);
   });
-  setTimeout(() => {}, 20000);
+  setTimeout(() => {
+    result.textContent = decimalToBinary(5);
+  }, 20000);
 };
 
 const checkUserInput = () => {
