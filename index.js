1284887b5049304655e6bed7d2e9a8c745e01be9
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-20 15:26:44 +0100

    99. Next, add the property showMsgDelay with the value 5000 and removeElDelay with the value 10000.

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const animationData = [
     inputVal: 1,
     addElDelay: 2000,
     msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
+    showMsgDelay: 5000,
+    removeElDelay: 10000,
   },
 ];
 
