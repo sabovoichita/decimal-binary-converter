ef4fffdfcb4554271e1320656b17a3196b1d9c16
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-20 15:47:19 +0100

    107. After the .forEach() method, add another setTimeout() function. Pass in an empty callback function as the first argument, and a delay of 20000 milliseconds as the second argument.

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ const showAnimation = () => {
       document.getElementById(obj.inputVal).remove();
     }, obj.removeElDelay);
   });
+  setTimeout(() => {}, 20000);
 };
 
 const checkUserInput = () => {
