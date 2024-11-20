f17bf5ec5720050d1d03bc859f80e9e8046d76da
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-20 15:25:27 +0100

    98. Set the value of the msg property to the following: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack."

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const animationData = [
   {
     inputVal: 1,
     addElDelay: 2000,
-    msg: "",
+    msg: "decimalToBinary(1) returns '1' (base case) and gives that value to the stack below. Then it pops off the stack.",
   },
 ];
 
